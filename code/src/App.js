24f190475179da7b1b5101f743e281ee744fa5c6
mmolliss <malin.vannesjo@gmail.com>
2021-03-08 14:49:22 +0100

    basic structure mobile first

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import data from './data.json'
+import React from 'react';
+import data from './data.json';
+import { Header } from './Components/Header.js';
+import { Album } from './Components/Album.js';
 
-console.log(data)
+console.log(data);
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Header />
+      <Album />
     </div>
-  )
-}
+  );
+};
