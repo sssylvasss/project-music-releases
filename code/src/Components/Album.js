f618f52444fc56bd9a0d367019253cd71a23e896
mmolliss <malin.vannesjo@gmail.com>
2021-03-09 12:52:06 +0100

    added icons

--- code/src/Components/Album.js
+++ code/src/Components/Album.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export const Album = (props) => {
+export const Album = (props) => { 
     return (
         <>
             <div className="album-container">
                 <div className="image-container">
                         <img className="albumcover" src={props.image} alt="Album cover" />
                     <div className="icon-container">
-                        <img className="heart-icon" src="code/public/icons/heart.svg" />
-                        <img className="play-icon" src="code/public/icons/play.svg" />
-                        <img className="dots-icon" src="code/public/icons/dots.svg" />
+                        <img className="heart-icon" src="/icons/heart.svg" />
+                        <img className="play-icon" src="/icons/play.svg" />
+                        <img className="dots-icon" src="/icons/dots.svg" />
                     </div>
                 </div>
                 <div className="text-container">
