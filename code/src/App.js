2c9b382683f6f39526bab67955dfc6242c3b25e7
mmolliss <malin.vannesjo@gmail.com>
2021-03-09 11:22:44 +0100

    map and props

--- code/src/App.js
+++ code/src/App.js
@@ -3,13 +3,26 @@ import data from './data.json';
 import { Header } from './Components/Header.js';
 import { Album } from './Components/Album.js';
 
-console.log(data);
+const albumArray = data.albums.items
 
 export const App = () => {
   return (
     <div>
       <Header />
       <Album />
+      <div> 
+        {albumArray.map(album => (
+          <Album
+          key={album.artists[0].id}
+              albumName={ album.name }
+              artistName={ album.artists[0].name }
+              image={ album.images[0].url }
+              albumURL={ album.external_urls.spotify }
+              artistURL={ album.artists[0].external_urls.spotify }
+              artists={ album.artists }
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- code/src/Components/Album.js
+++ code/src/Components/Album.js
@@ -1,15 +1,20 @@
 import React from 'react';
-// import data from 'code/src/data.json';
 
-export const Album = () => {
-    const imageSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYsdNzi5PAtg3hlPVFlzQXPLIAW_5hcPrFmw&usqp=CAU";
+export const Album = (props) => {
     return (
         <>
             <div className="album-container">
-                <img className="albumcover" src={imageSrc} alt="Album cover" />
+                <div className="image-container">
+                        <img className="albumcover" src={props.image} alt="Album cover" />
+                    <div className="icon-container">
+                        <img className="heart-icon" src="/Users/mm/Development/technigo/wk9/project-music-releases/code/public/icons/heart.svg" />
+                        <img className="play-icon" src="code/public/icons/play.svg" />
+                        <img className="dots-icon" src="code/public/icons/dots.svg" />
+                    </div>
+                </div>
                 <div className="text-container">
-                    <h2 className="album-name">hejhej</h2>
-                    <p className="artist-name">hel</p>
+                    <h2 className="album-name">{props.albumName}</h2>
+                    <p className="artist-name">{props.artistName}</p>
                 </div>
             </div>
         </>
