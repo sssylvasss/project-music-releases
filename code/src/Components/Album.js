10025907c7d87b3070bf757d79fa732c2d189164
mmolliss <malin.vannesjo@gmail.com>
2021-03-11 11:25:56 +0100

    Playing around with the styling of the hovers

--- code/src/Components/Album.js
+++ code/src/Components/Album.js
@@ -6,7 +6,7 @@ export const Album = (props) => {
             <div className="album-container">
                 <div className="image-container">
                     <img className="albumcover" src={props.image} alt="Album cover" />
-                    <div className="icon-container">
+                    <div className="icon-container hidden">
                         <img className="heart-icon" src="/icons/heart.svg" />
                         <img className="play-icon" src="/icons/play.svg" />
                         <img className="dots-icon" src="/icons/dots.svg" />
